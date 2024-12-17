1565ec4d89b547f515c1a1415d58abc24f97c6ce
su-su-su-su <suzukikoji0503@gmail.com>
2024-12-17 17:48:11 +0900

    毎月の受付設定にt定休日を表示

--- app/models/holiday.rb
+++ app/models/holiday.rb
@@ -2,4 +2,12 @@
 
 class Holiday < ApplicationRecord
   belongs_to :stylist, class_name: 'User'
+
+  def self.default_for(stylist_id, date)
+    holiday = find_by(stylist_id: stylist_id, target_date: date)
+    return holiday if holiday.present?
+
+    wday = date.wday
+    find_by(stylist_id: stylist_id, day_of_week: wday, target_date: nil)
+  end
 end
