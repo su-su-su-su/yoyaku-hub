93b39980daf973937e974e51c1f9278804ad3f91
su-su-su-su <suzukikoji0503@gmail.com>
2025-01-01 22:10:46 +0900

    メニュー表示をis_activeがtrueのみに修正

--- app/controllers/customers/stylists/menus_controller.rb
+++ app/controllers/customers/stylists/menus_controller.rb
@@ -5,7 +5,7 @@ module Customers
     class MenusController < ApplicationController
       def index
         @stylist = User.find(params[:stylist_id])
-        @menus = @stylist.menus
+        @menus = @stylist.menus.where(is_active: true)
       end
     end
   end
