e044094246a22bdfaefa1c272c3ebc72c9250342
su-su-su-su <suzukikoji0503@gmail.com>
2025-01-16 11:47:05 +0900

    Reservation、Userモデルを修正、rubocopに対する修正

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 class Reservation < ApplicationRecord
-  belongs_to :stylist, class_name: 'User'
-  belongs_to :customer, class_name: 'User'
+  belongs_to :customer, class_name: 'User', inverse_of: :reservations
+
+  belongs_to :stylist, class_name: 'User', inverse_of: :stylist_reservations
 
   has_many :reservation_menu_selections, dependent: :destroy
   has_many :menus, through: :reservation_menu_selections
