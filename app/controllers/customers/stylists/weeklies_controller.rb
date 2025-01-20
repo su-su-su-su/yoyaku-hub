4e05fdfc69aee4b2c8924e03ea04f7dabafaff3d
su-su-su-su <suzukikoji0503@gmail.com>
2025-01-20 23:49:53 +0900

    予約の前後に隙間時間ができる場合、△が表示されるように変更

--- app/controllers/customers/stylists/weeklies_controller.rb
+++ app/controllers/customers/stylists/weeklies_controller.rb
@@ -3,7 +3,8 @@
 module Customers
   module Stylists
     class WeekliesController < ApplicationController
-      helper_method :total_duration, :within_working_hours?
+      helper_method :total_duration, :within_working_hours?, :time_range_occupied?
+
       def index
         set_stylist
         set_selected_menus
@@ -12,10 +13,11 @@ module Customers
         fetch_working_hours
         fetch_holidays
         filter_non_holiday_working_hours
-        calculate_time_slots
         build_working_hours_hash
         build_reservation_limits_hash
         set_can_go_previous
+        prepare_occupied_slots
+        @time_slots = build_time_slots_for_week(@dates)
       end
 
       private
@@ -26,7 +28,7 @@ module Customers
 
       def set_selected_menus
         @selected_menu_ids = params[:menu_ids] || []
-        @selected_menus    = @stylist.menus.where(id: @selected_menu_ids)
+        @selected_menus = @stylist.menus.where(id: @selected_menu_ids)
       end
 
       def set_dates_and_time_slots
@@ -52,18 +54,6 @@ module Customers
 
       def filter_non_holiday_working_hours
         @wh_non_holiday = @wh_list.reject { |wh| @holiday_days.include?(wh.target_date) }
-      end
-
-      def calculate_time_slots
-        earliest_start = @wh_non_holiday.map(&:start_time).compact_blank.min
-        latest_end     = @wh_non_holiday.map(&:end_time).compact_blank.max
-
-        @time_slots = []
-        current_time = earliest_start
-        while current_time < latest_end
-          @time_slots << current_time.strftime('%H:%M')
-          current_time += 30.minutes
-        end
       end
 
       def build_working_hours_hash
@@ -108,11 +98,58 @@ module Customers
 
         day_start_hm = working_hours.start_time.strftime('%H:%M')
         day_end_hm = working_hours.end_time.strftime('%H:%M')
-
         start_time = Time.zone.parse("#{date} #{time_str}")
         end_time   = start_time + total_minutes.minutes
-
         (time_str >= day_start_hm) && (end_time.strftime('%H:%M') <= day_end_hm)
+      end
+
+      def prepare_occupied_slots
+        @occupied_slots_hash = {}
+        @dates.each do |d|
+          @occupied_slots_hash[d] = {}
+        end
+
+        @reservations_for_week = Reservation.where(
+          stylist_id: @stylist.id,
+          start_at: @dates.first.beginning_of_day..@dates.last.end_of_day
+        )
+
+        @reservations_for_week.each do |res|
+          date = res.start_at.to_date
+          next unless @occupied_slots_hash.key?(date)
+
+          start_s = slot_for_time(res.start_at)
+          end_s = slot_for_time(res.end_at)
+
+          (start_s...end_s).each do |s|
+            @occupied_slots_hash[date][s] = true
+          end
+        end
+      end
+
+      def build_time_slots_for_week(_dates)
+        earliest_start = @wh_non_holiday.map(&:start_time).compact_blank.min
+        latest_end = @wh_non_holiday.map(&:end_time).compact_blank.max
+
+        return [] if earliest_start.blank? || latest_end.blank?
+
+        slots = []
+        current_time = earliest_start
+        while current_time < latest_end
+          slots << current_time.strftime('%H:%M')
+          current_time += 30.minutes
+        end
+        slots
+      end
+
+      def slot_for_time(time)
+        (time.hour * 2) + (time.min >= 30 ? 1 : 0)
+      end
+
+      def time_range_occupied?(date, start_slot, needed_slots)
+        (start_slot...(start_slot + needed_slots)).any? do |s|
+          @occupied_slots_hash[date][s] == true
+        end
       end
     end
   end
