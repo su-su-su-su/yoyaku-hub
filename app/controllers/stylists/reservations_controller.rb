94c847896c2fffedce1f48c6256129be82d5793a
su-su-su-su <suzukikoji0503@gmail.com>
2025-02-03 23:25:31 +0900

    予約をクリックすると予約の詳細が表示されるように実装

--- app/controllers/stylists/reservations_controller.rb
+++ app/controllers/stylists/reservations_controller.rb
@@ -1,2 +1,9 @@
-class Stylists::ReservationsController < ApplicationController
+# frozen_string_literal: true
+
+module Stylists
+  class ReservationsController < ApplicationController
+    def show
+      @reservation = Reservation.find(params[:id])
+    end
+  end
 end
