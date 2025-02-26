73db32a92049074d066c4ca7d6faa7c8c433b7a1
su-su-su-su <suzukikoji0503@gmail.com>
2025-02-26 23:01:54 +0900

    WorkingHourモデルのリファクタリングに合わせ、ShiftSettingsController、ReservationsControllerの時間オプション生成処理を共通メソッドに統一

--- app/controllers/stylists/shift_settings_controller.rb
+++ app/controllers/stylists/shift_settings_controller.rb
@@ -4,65 +4,31 @@ module Stylists
   class ShiftSettingsController < StylistsController
     before_action :authenticate_user!
     before_action -> { ensure_role(:stylist) }
-    before_action :set_time_options, only: [:index, :show]
+    before_action :set_date_info, only: [:index]
 
     def index
-      wday_example = 1
-      weekday_wh = WorkingHour.find_by(stylist_id: current_user.id, day_of_week: wday_example)
-      if weekday_wh
-        @weekday_start_str = weekday_wh.start_time.strftime('%H:%M')
-        @weekday_end_str = weekday_wh.end_time.strftime('%H:%M')
-      else
-        @weekday_start_str = '09:00'
-        @weekday_end_str = '18:00'
-      end
+      @weekday_hours = WorkingHour.formatted_hours(WorkingHour.find_by(stylist_id: current_user.id, day_of_week: 1))
+      @saturday_hours = WorkingHour.formatted_hours(WorkingHour.find_by(stylist_id: current_user.id, day_of_week: 6))
+      @sunday_hours = WorkingHour.formatted_hours(WorkingHour.find_by(stylist_id: current_user.id, day_of_week: 0))
+      @holiday_hours = WorkingHour.formatted_hours(WorkingHour.find_by(stylist_id: current_user.id, day_of_week: 7))
 
-      sat_wh = WorkingHour.find_by(stylist_id: current_user.id, day_of_week: 6)
-      if sat_wh
-        @saturday_start_str = sat_wh.start_time.strftime('%H:%M')
-        @saturday_end_str = sat_wh.end_time.strftime('%H:%M')
-      else
-        @saturday_start_str = '09:00'
-        @saturday_end_str = '18:00'
-      end
-
-      sun_wh = WorkingHour.find_by(stylist_id: current_user.id, day_of_week: 0)
-      if sun_wh
-        @sunday_start_str = sun_wh.start_time.strftime('%H:%M')
-        @sunday_end_str = sun_wh.end_time.strftime('%H:%M')
-      else
-        @sunday_start_str = '09:00'
-        @sunday_end_str = '18:00'
-      end
-
-      holiday_wh = WorkingHour.find_by(stylist_id: current_user.id, day_of_week: 7)
-      if holiday_wh
-        @holiday_start_str = holiday_wh.start_time.strftime('%H:%M')
-        @holiday_end_str = holiday_wh.end_time.strftime('%H:%M')
-      else
-        @holiday_start_str = '09:00'
-        @holiday_end_str = '18:00'
-      end
+      @weekday_start_str = @weekday_hours[:start]
+      @weekday_end_str = @weekday_hours[:end]
+      @saturday_start_str = @saturday_hours[:start]
+      @saturday_end_str = @saturday_hours[:end]
+      @sunday_start_str = @sunday_hours[:start]
+      @sunday_end_str = @sunday_hours[:end]
+      @holiday_start_str = @holiday_hours[:start]
+      @holiday_end_str = @holiday_hours[:end]
 
       @chosen_wdays = Holiday.where(stylist_id: current_user.id).pluck(:day_of_week)
-
       @current_limit = ReservationLimit.find_by(stylist_id: current_user.id, target_date: nil)
-
-      today = Date.today
-      @this_month_year = today.year
-      @this_month = today.month
-
-      next_month_date = today.next_month
-      @next_month_year = next_month_date.year
-      @next_month = next_month_date.month
-
-      next_next_month_date = next_month_date.next_month
-      @next_next_month_year = next_next_month_date.year
-      @next_next_month = next_next_month_date.month
 
       @is_this_month_configured = month_configured?(@this_month_year, @this_month)
       @is_next_month_configured = month_configured?(@next_month_year, @next_month)
       @is_next_next_month_configured = month_configured?(@next_next_month_year, @next_next_month)
+
+      @time_options = WorkingHour.full_time_options
     end
 
     def show
@@ -85,6 +51,8 @@ module Stylists
       (@start_date..@start_date.end_of_month).each do |date|
         @reservation_limits_for_month[date] = ReservationLimit.default_for(current_user.id, date)
       end
+
+      @time_options = WorkingHour.full_time_options
     end
 
     def create
@@ -95,8 +63,8 @@ module Stylists
         date = Date.parse(date_str) rescue nil
         is_holiday = (day_values[:is_holiday] == "1")
 
-        start_str = day_values[:start_time].presence || "09:00"
-        end_str = day_values[:end_time].presence || "18:00"
+        start_str = day_values[:start_time].presence || WorkingHour::DEFAULT_START_TIME
+        end_str = day_values[:end_time].presence || WorkingHour::DEFAULT_END_TIME
         max_res_str = day_values[:max_reservations].presence || "2"
 
         if is_holiday
@@ -127,26 +95,7 @@ module Stylists
         wh.holiday_flag = is_holiday ? "1" : "0"
         wh.save!
 
-        rl_day = ReservationLimit.find_or_initialize_by(
-          stylist_id: current_user.id,
-          target_date: date,
-          time_slot: nil
-        )
-        rl_day.max_reservations = max_res_str.to_i
-        rl_day.save!
-
-        start_slot = (start_time_obj.hour * 2) + (start_time_obj.min >= 30 ? 1 : 0)
-        end_slot = (end_time_obj.hour * 2) + (end_time_obj.min >= 30 ? 1 : 0)
-
-        (start_slot...end_slot).each do |slot|
-          rl_slot = ReservationLimit.find_or_initialize_by(
-            stylist_id: current_user.id,
-            target_date: date,
-            time_slot: slot
-          )
-          rl_slot.max_reservations = max_res_str.to_i
-          rl_slot.save!
-        end
+        save_reservation_limits(date, start_time_obj, end_time_obj, max_res_str.to_i)
       end
 
       redirect_to stylists_shift_settings_path, notice: I18n.t('flash.batch_setting_success')
@@ -154,23 +103,49 @@ module Stylists
 
     private
 
-      def set_time_options
-        @time_options = (0..47).map do |i|
-          hour = i / 2
-          minute = (i % 2) * 30
-          time_str = format('%<hour>02d:%<minute>02d', hour: hour, minute: minute)
-          [time_str, time_str]
-        end
-      end
+    def set_date_info
+      today = Date.today
+      @this_month_year = today.year
+      @this_month = today.month
+      next_month_date = today.next_month
+      @next_month_year = next_month_date.year
+      @next_month = next_month_date.month
+      next_next_month_date = next_month_date.next_month
+      @next_next_month_year = next_next_month_date.year
+      @next_next_month = next_next_month_date.month
+    end
 
-      def month_configured?(year, month)
-        start_date = Date.new(year, month, 1)
-        end_date = start_date.end_of_month
+    def save_reservation_limits(date, start_time_obj, end_time_obj, max_reservations)
+      rl_day = ReservationLimit.find_or_initialize_by(
+        stylist_id: current_user.id,
+        target_date: date,
+        time_slot: nil
+      )
+      rl_day.max_reservations = max_reservations
+      rl_day.save!
 
-        working_hour_exists = WorkingHour.where(stylist_id: current_user.id, target_date: start_date..end_date).exists?
-        holiday_exists = Holiday.where(stylist_id: current_user.id, target_date: start_date..end_date).exists?
-        reservation_limit_exist = ReservationLimit.where(stylist_id: current_user.id, target_date: start_date..end_date).exists?
-        working_hour_exists || holiday_exists || reservation_limit_exist
+      start_slot = (start_time_obj.hour * 2) + (start_time_obj.min >= 30 ? 1 : 0)
+      end_slot = (end_time_obj.hour * 2) + (end_time_obj.min >= 30 ? 1 : 0)
+
+      (start_slot...end_slot).each do |slot|
+        rl_slot = ReservationLimit.find_or_initialize_by(
+          stylist_id: current_user.id,
+          target_date: date,
+          time_slot: slot
+        )
+        rl_slot.max_reservations = max_reservations
+        rl_slot.save!
       end
+    end
+
+    def month_configured?(year, month)
+      start_date = Date.new(year, month, 1)
+      end_date = start_date.end_of_month
+
+      working_hour_exists = WorkingHour.where(stylist_id: current_user.id, target_date: start_date..end_date).exists?
+      holiday_exists = Holiday.where(stylist_id: current_user.id, target_date: start_date..end_date).exists?
+      reservation_limit_exist = ReservationLimit.where(stylist_id: current_user.id, target_date: start_date..end_date).exists?
+      working_hour_exists || holiday_exists || reservation_limit_exist
+    end
   end
 end
--- app/models/working_hour.rb
+++ app/models/working_hour.rb
@@ -4,6 +4,9 @@ class WorkingHour < ApplicationRecord
   belongs_to :stylist, class_name: 'User'
   attr_accessor :holiday_flag
 
+  DEFAULT_START_TIME = '09:00'
+  DEFAULT_END_TIME = '18:00'
+
   validates :start_time, presence: true
   validates :end_time, presence: true
   validate :end_time_after_start_time, unless: :holiday_flag?
@@ -24,13 +27,60 @@ class WorkingHour < ApplicationRecord
     new(
       stylist_id: stylist_id,
       target_date: date,
-      start_time: Time.zone.parse('09:00'),
-      end_time: Time.zone.parse('18:00')
+      start_time: Time.zone.parse(DEFAULT_START_TIME),
+      end_time: Time.zone.parse(DEFAULT_END_TIME)
     )
   end
 
   def self.date_only_for(stylist_id, date)
     find_by(stylist_id: stylist_id, target_date: date)
+  end
+
+  def self.full_time_options
+    (0..47).map do |i|
+      hour = i / 2
+      minute = (i % 2) * 30
+      time_str = format('%<hour>02d:%<minute>02d', hour: hour, minute: minute)
+      [time_str, time_str]
+    end
+  end
+
+  def self.time_options_for(stylist_id, date)
+    working_hour = date_only_for(stylist_id, date)
+
+    if working_hour.present?
+      start_time = working_hour.start_time
+      end_time = working_hour.end_time
+    else
+      start_time = Time.zone.parse(DEFAULT_START_TIME)
+      end_time = Time.zone.parse(DEFAULT_END_TIME)
+    end
+
+    generate_time_options_between(start_time, end_time)
+  end
+
+  def self.generate_time_options_between(start_time, end_time)
+    time_options = []
+    current_time = start_time
+
+    while current_time <= end_time
+      formatted = current_time.strftime('%H:%M')
+      time_options << [formatted, formatted]
+      current_time += 30.minutes
+    end
+
+    time_options
+  end
+
+  def self.formatted_hours(working_hour)
+    if working_hour.present?
+      {
+        start: working_hour.start_time.strftime('%H:%M'),
+        end: working_hour.end_time.strftime('%H:%M')
+      }
+    else
+      { start: DEFAULT_START_TIME, end: DEFAULT_END_TIME }
+    end
   end
 
   def holiday_flag?
