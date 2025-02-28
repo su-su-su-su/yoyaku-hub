14337d289543453290b2a943d953d45e614f1de3
su-su-su-su <suzukikoji0503@gmail.com>
2025-02-28 22:08:51 +0900

    ReservationsControllerとReservationモデルをリファクタリング

--- app/controllers/customers/reservations_controller.rb
+++ app/controllers/customers/reservations_controller.rb
@@ -4,92 +4,47 @@ module Customers
   class ReservationsController < ApplicationController
     before_action :authenticate_user!
     before_action -> { ensure_role(:customer) }
-    before_action :set_reservation, only: [:show]
+    before_action :set_reservation, only: [:show, :cancel]
+    before_action :set_reservation_form_data, only: [:new, :create]
 
     def index
       user = current_user
-      @upcoming_reservations = user.reservations.where("start_at >= ?", Time.zone.now).where.not(status: [:canceled, :no_show]).order(:start_at)
-      @past_reservations = user.reservations.where("(start_at < :now) OR (status = :canceled)",now: Time.zone.now, canceled: Reservation.statuses[:canceled]).order(start_at: :desc)
+      @upcoming_reservations = user.reservations
+                                   .where("start_at >= ?", Time.zone.now)
+                                   .where.not(status: [:canceled, :no_show])
+                                   .order(:start_at)
+
+      @past_reservations = user.reservations
+                               .where("(start_at < :now) OR (status = :canceled)",
+                                      now: Time.zone.now,
+                                      canceled: Reservation.statuses[:canceled])
+                               .order(start_at: :desc)
     end
 
     def new
-      @stylist_id = params[:stylist_id]
-      @date = params[:date]
-      @time_str = params[:time_str]
-      @menu_ids = params[:menu_ids]
-
-      @stylist = User.find(@stylist_id)
-
       @user = current_user
-
-      @menus = Menu.where(id: @menu_ids).to_a
-
-      @total_duration = @menus.sum(&:duration)
-      @total_price  = @menus.sum(&:price)
-
-      date_time_str = "#{@date} #{@time_str}"
-      @start_time_obj = begin
-        Time.zone.parse(date_time_str)
-      rescue StandardError
-        nil
-      end
+      parse_start_time_for_display
     end
 
     def show
-      @stylist = @reservation.stylist
-      @menus   = @reservation.menus
-
-      @total_duration = @menus.sum(&:duration)
-      @total_price    = @menus.sum(&:price)
+      load_reservation_details
     end
 
     def create
-      stylist_id = params[:stylist_id]
-      date = params[:date]
-      time_str = params[:time_str]
-      menu_ids = params[:menu_ids]
+      @user = current_user
 
-      stylist = User.find(stylist_id)
-      user = current_user
-      menus = Menu.where(id: menu_ids)
+      @reservation = build_reservation
 
-      total_duration = menus.sum(&:duration)
-      menus.sum(&:price)
-
-      start_time_obj = begin
-        Time.zone.parse("#{date} #{time_str}")
-      rescue StandardError
-        nil
-      end
-      end_time_obj = (start_time_obj + total_duration.minutes if start_time_obj && total_duration)
-
-      @reservation = Reservation.new(
-        customer_id: user.id,
-        stylist_id: stylist.id,
-        start_at: start_time_obj,
-        end_at: end_time_obj
-      )
-
-      @reservation.menu_ids = menu_ids
       if @reservation.save
-
-        redirect_to customers_reservation_path(@reservation), notice: I18n.t('flash.reservation_confirmed')
+        redirect_to customers_reservation_path(@reservation),
+                    notice: I18n.t('flash.reservation_confirmed')
       else
-        @stylist = stylist
-        @menus = menus
-        @date = date
-        @time_str = time_str
-        @menu_ids = menu_ids
-        @total_duration = total_duration
-        @total_price = menus.sum(&:price)
-        @start_time_obj = start_time_obj
-        flash.now[:alert] = '予約の保存に失敗しました。'
+        flash.now[:alert] = I18n.t('flash.reservation_failed')
         render :new
       end
     end
 
     def cancel
-      @reservation = Reservation.find(params[:id])
       @reservation.canceled!
       redirect_to customers_reservations_path(date: @reservation.start_at.to_date),
                   notice: I18n.t('flash.reservation_cancelled')
@@ -100,7 +55,48 @@ module Customers
     def set_reservation
       @reservation = current_user.reservations.find(params[:id])
     rescue ActiveRecord::RecordNotFound
-      redirect_to customers_reservations_path, alert: "予約が見つかりません。"
+      redirect_to customers_reservations_path, alert: I18n.t('flash.reservation_not_found')
+    end
+
+    def set_reservation_form_data
+      @stylist_id = params[:stylist_id]
+      @date     = params[:date]
+      @time_str = params[:time_str]
+      @menu_ids = params[:menu_ids]
+      @stylist  = User.find(@stylist_id)
+      @menus    = Menu.where(id: @menu_ids).to_a
+      calculate_totals
+    end
+
+    def parse_start_time_for_display
+      date_time_str = "#{@date} #{@time_str}"
+      @start_time_obj = begin
+        Time.zone.parse(date_time_str)
+      rescue StandardError
+        nil
+      end
+    end
+
+    def calculate_totals
+      @total_duration = @menus.sum(&:duration)
+      @total_price    = @menus.sum(&:price)
+    end
+
+    def load_reservation_details
+      @stylist = @reservation.stylist
+      @menus   = @reservation.menus
+      calculate_totals
+    end
+
+    def build_reservation
+      reservation = Reservation.new(
+        customer:         @user,
+        stylist:          @stylist,
+        start_date_str:   @date,
+        start_time_str:   @time_str
+      )
+      reservation.menu_ids = @menu_ids
+      reservation
     end
   end
 end
