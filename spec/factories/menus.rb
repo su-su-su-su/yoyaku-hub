1cfa2bc7602af4054299ab27771826f8e0e65a09
su-su-su-su <suzukikoji0503@gmail.com>
2025-03-04 22:14:18 +0900

    Menuファクトリのトレイトを追加

--- spec/factories/menus.rb
+++ spec/factories/menus.rb
@@ -2,13 +2,62 @@
 
 FactoryBot.define do
   factory :menu do
-    stylist { nil }
-    name { 'MyString' }
-    price { 1 }
-    duration { 1 }
-    description { 'MyText' }
-    category { 'MyString' }
-    sort_order { 1 }
-    is_active { false }
+    sequence(:name) { |n| "メニュー#{n}" }
+    price { 5500 }
+    duration { 60 }
+    sequence(:description) { |n| "メニュー#{n}です" }
+    category { %w[カット] }
+    is_active { true }
+    sequence(:sort_order) { |n| n }
+
+    association :stylist, factory: %i[user stylist]
+
+    trait :low_price do
+      price { 3000 }
+    end
+
+    trait :high_price do
+      price { 10_000 }
+    end
+
+    trait :short_duration do
+      duration { 30 }
+    end
+
+    trait :long_duration do
+      duration { 120 }
+    end
+
+    trait :cut do
+      category { %w[カット] }
+      duration { 60 }
+      price { 6600 }
+    end
+
+    trait :color do
+      category { %w[カラー] }
+      duration { 90 }
+      price { 8000 }
+    end
+
+    trait :perm do
+      category { %w[パーマ] }
+      duration { 100 }
+      price { 9000 }
+    end
+
+    trait :multi_category do
+      category { %w[カット カラー] }
+      duration { 120 }
+      price { 12_000 }
+    end
+
+    trait :inactive do
+      is_active { false }
+    end
+
+    trait :without_description do
+      description { nil }
+    end
   end
 end
