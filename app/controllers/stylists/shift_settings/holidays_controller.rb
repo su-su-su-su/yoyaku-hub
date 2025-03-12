1435f6fdc16dbe075d938d950f8dbf9bf07b1890
su-su-su-su <suzukikoji0503@gmail.com>
2025-03-12 13:08:38 +0900

    全ての休業日を解除できるように修正

--- app/controllers/stylists/shift_settings/holidays_controller.rb
+++ app/controllers/stylists/shift_settings/holidays_controller.rb
@@ -7,15 +7,19 @@ module Stylists
 
       def create
         holiday_params = params.require(:holiday).permit(day_of_weeks: [])
-        chosen_wdays = holiday_params[:day_of_weeks].map(&:to_i)
+        chosen_wdays = holiday_params[:day_of_weeks].compact_blank.map(&:to_i)
 
         existing = Holiday.where(stylist_id: current_user.id)
 
-        existing.where.not(day_of_week: chosen_wdays).destroy_all
+        if chosen_wdays.empty?
+          existing.destroy_all
+        else
+          existing.where.not(day_of_week: chosen_wdays).destroy_all
 
-        chosen_wdays.each do |wday|
-          hol = Holiday.find_or_initialize_by(stylist_id: current_user.id, day_of_week: wday)
-          hol.save! unless hol.persisted?
+          chosen_wdays.each do |wday|
+            hol = Holiday.find_or_initialize_by(stylist_id: current_user.id, day_of_week: wday)
+            hol.save! unless hol.persisted?
+          end
         end
 
         redirect_to stylists_shift_settings_path, notice: I18n.t('stylists.shift_settings.holidays.create_success')
