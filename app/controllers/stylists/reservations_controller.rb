29f35c5d4771ab7a331a5399d932d1f6bf95b6ee
su-su-su-su <suzukikoji0503@gmail.com>
2025-03-25 01:40:53 +0900

    stylists/reservations_controller.rbのコードスタイルの一貫性確保とセキュリティ対策の強化

--- app/controllers/stylists/reservations_controller.rb
+++ app/controllers/stylists/reservations_controller.rb
@@ -4,16 +4,13 @@ module Stylists
   class ReservationsController < ApplicationController
     before_action :authenticate_user!
     before_action -> { ensure_role(:stylist) }
-    before_action :set_reservation, only: %i[edit update]
-    before_action :prepare_time_options, only: %i[edit update]
-    before_action :set_active_menus, only: %i[edit update]
+    before_action :set_reservation, only: %i[show edit update cancel]
+    before_action :load_time_options, only: %i[edit update]
+    before_action :load_active_menus, only: %i[edit update]
 
-    def show
-      @reservation = Reservation.find(params[:id])
-    end
+    def show; end
 
     def cancel
-      @reservation = Reservation.find(params[:id])
       @reservation.canceled!
       redirect_to stylists_schedules_path(date: @reservation.start_at.to_date), notice: t('stylists.reservations.cancelled')
     end
@@ -30,7 +27,7 @@ module Stylists
 
     def update_time_options
       reservation_date = params[:start_date_str].present? ? Date.parse(params[:start_date_str]) : Time.zone.today
-      stylist_id = params[:stylist_id]
+      stylist_id = current_user.id
 
       @time_options = WorkingHour.time_options_for(stylist_id, reservation_date)
 
@@ -40,14 +37,14 @@ module Stylists
     private
 
     def set_reservation
-      @reservation = Reservation.find(params[:id])
+      @reservation = current_user.stylist_reservations.find(params[:id])
     end
 
     def reservation_params
       params.require(:reservation).permit(:start_date_str, :start_time_str, :custom_duration, menu_ids: [])
     end
 
-    def prepare_time_options
+    def load_time_options
       reservation_date = if params.dig(:reservation, :start_date_str).present?
                            Date.parse(params[:reservation][:start_date_str])
                          elsif @reservation.start_at.present?
@@ -56,11 +53,11 @@ module Stylists
                            Time.zone.today
                          end
 
-      stylist_id = @reservation.stylist_id
+      stylist_id = current_user.id
       @time_options = WorkingHour.time_options_for(stylist_id, reservation_date)
     end
 
-    def set_active_menus
+    def load_active_menus
       @active_menus = current_user.menus.where(is_active: true)
     end
   end
