f77925cb233f2592bbebc82a0b2ff20b37485249
su-su-su-su <suzukikoji0503@gmail.com>
2025-04-09 13:14:00 +0900

    rubocopに対応修正

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -1,18 +1,22 @@
-class Users::PasswordsController < Devise::PasswordsController
-  def create
-    self.resource = resource_class.send_reset_password_instructions(resource_params)
-    yield resource if block_given?
+# frozen_string_literal: true
 
-    if successfully_sent?(resource)
-      respond_to do |format|
-        format.html do
-          redirect_to after_sending_reset_password_instructions_path_for(resource_name),
-            status: :see_other, notice: t('devise.passwords.send_instructions')
+module Users
+  class PasswordsController < Devise::PasswordsController
+    def create
+      self.resource = resource_class.send_reset_password_instructions(resource_params)
+      yield resource if block_given?
+
+      if successfully_sent?(resource)
+        respond_to do |format|
+          format.html do
+            redirect_to after_sending_reset_password_instructions_path_for(resource_name),
+              status: :see_other, notice: t('devise.passwords.send_instructions')
+          end
         end
-      end
-    else
-      respond_to do |format|
-        format.html { render :new, status: :unprocessable_entity }
+      else
+        respond_to do |format|
+          format.html { render :new, status: :unprocessable_entity }
+        end
       end
     end
   end
