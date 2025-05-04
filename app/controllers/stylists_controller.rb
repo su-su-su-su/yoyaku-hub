eda4c7fb6c6979c2ee6338555a5f781922f29a09
su-su-su-su <suzukikoji0503@gmail.com>
2025-05-04 23:27:05 +0900

    今月、来月の受付設定を確認するメソッドを実装できるようにした

--- app/controllers/stylists_controller.rb
+++ app/controllers/stylists_controller.rb
@@ -5,14 +5,33 @@ class StylistsController < ApplicationController
   before_action :ensure_stylist_role
 
   def show
-    @show_shift_settings_warning = !current_user.default_shift_settings_configured?
-    @show_menu_warning = !current_user.has_registered_menus?
     @stylist_id = current_user.id
+    @show_shift_settings_warning = !current_user.default_shift_settings_configured?
+    @show_menu_warning = !current_user.registered_menus?
+    @show_current_month_setup_prompt = !current_user.current_month_shifts_configured?
+    @show_next_month_setup_prompt = false
+    is_next_month_unconfigured = !current_user.next_month_shifts_configured?
+    is_after_20th = Date.current.day > 20
+
+    @show_next_month_setup_prompt = true if is_next_month_unconfigured && is_after_20th
+    set_date_info_for_dashboard
   end
 
   private
 
   def ensure_stylist_role
     ensure_role('stylist')
+  end
+
+  def set_date_info_for_dashboard
+    today = Time.zone.today
+    @this_month_year = today.year
+    @this_month = today.month
+    next_month_date = today.next_month
+    @next_month_year = next_month_date.year
+    @next_month = next_month_date.month
+    next_next_month_date = next_month_date.next_month
+    @next_next_month_year = next_next_month_date.year
+    @next_next_month = next_next_month_date.month
   end
 end
