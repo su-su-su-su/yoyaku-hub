4c779ba51aaa38f2fc33d2cd562bbe519146e562
su-su-su-su <suzukikoji0503@gmail.com>
2025-05-13 15:46:13 +0900

    ログアウトした時にログインページにリダイレクトされるようにした

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -36,4 +36,8 @@ class ApplicationController < ActionController::Base
   def render_not_found
     render file: Rails.public_path.join('404.html'), status: :not_found, layout: false
   end
+
+  def after_sign_out_path_for(_resource_or_scope)
+    new_user_session_path
+  end
 end
