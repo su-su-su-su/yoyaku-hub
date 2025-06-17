504091e0407dc782244ff0135a33a5865c149784
su-su-su-su <suzukikoji0503@gmail.com>
2025-06-17 14:50:04 +0900

    不要になったrender_navbarヘルパーを削除

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,18 +1,6 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
-  def render_navbar
-    if user_signed_in?
-      if current_user.stylist?
-        render 'layouts/navbars/stylist_navbar'
-      else
-        render 'layouts/navbars/customer_navbar'
-      end
-    else
-      render 'layouts/navbars/guest_navbar'
-    end
-  end
-
   def menu_form_url(menu)
     if menu.new_record?
       menus_settings_path
