b47b6e187efb6f8273c101c6f654a4b33215135f
su-su-su-su <suzukikoji0503@gmail.com>
2025-07-29 17:26:59 +0900

    顧客予約登録フォームのUI実装

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -5,6 +5,7 @@ import flash_controller from "./flash_controller.js";
 import CopyClipboardController from "./copy_clipboard_controller.js";
 import ScheduleDatePickerController from "./schedule_date_picker_controller.js";
 import AccountingController from "./accounting_controller.js";
+import CustomerSearchController from "./customer_search_controller.js";
 
 application.register("holiday-toggle", HolidayToggleController);
 application.register("time-options", TimeOptionsController);
@@ -12,3 +13,4 @@ application.register("flash", flash_controller);
 application.register("copy-clipboard", CopyClipboardController);
 application.register("schedule-date-picker", ScheduleDatePickerController);
 application.register("accounting", AccountingController);
+application.register("customer-search", CustomerSearchController);
