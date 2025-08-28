1fef7a3fbcdbbd4a13ec85215fe03dca7352278c
su-su-su-su <suzukikoji0503@gmail.com>
2025-08-28 23:36:20 +0900

    ログインエラー時にトースト通知を表示
    - SessionsControllerでログイン失敗時の処理を実装
    - wardenの認証結果に応じてトースト通知を表示
    - ログインエラーテストにトースト通知の確認を追加
    - RuboCopのAbcSize警告を無効化

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,15 +2,27 @@
 
 module Users
   class SessionsController < Devise::SessionsController
+    # rubocop:disable Metrics/AbcSize
     def create
-      super do |resource|
-        redirect_path = after_sign_in_path_for(resource)
+      self.resource = warden.authenticate(auth_options)
 
+      if resource
+        # ログイン成功
+        set_flash_message!(:notice, :signed_in)
+        sign_in(resource_name, resource)
         flash[:toast] = { message: I18n.t('devise.sessions.signed_in'), type: :success }
-
-        redirect_to redirect_path, status: :see_other and return
+        respond_with resource, location: after_sign_in_path_for(resource)
+      else
+        # ログイン失敗
+        self.resource = resource_class.new(sign_in_params)
+        clean_up_passwords(resource)
+        flash.now[:toast] = { message: I18n.t('devise.failure.invalid', authentication_keys: 'メールアドレス'), type: :error }
+        respond_with(resource) do |format|
+          format.html { render :new, status: :unprocessable_entity }
+        end
       end
     end
+    # rubocop:enable Metrics/AbcSize
 
     def destroy
       super do
